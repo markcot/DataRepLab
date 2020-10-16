013c21cc9285e3b80f2123673f7c0b05482a39fd
markcot <g00376335@gmit.ie>
2020-10-16 19:47:58 +0100

    L3b step 6

--- week03-webScraping/PY03-readOurFile.py
+++ week03-webScraping/PY03-readOurFile.py
@@ -3,4 +3,11 @@ from bs4 import BeautifulSoup
 with open("../carviewer.html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
-print(soup.prettify())
+#print(soup.tr)
+rows = soup.findAll("tr")
+for row in rows:
+    print("------")
+    #print(row)
+    cols = row.findAll("td")
+    for col in cols:
+        print(col.text)
--- week03-webScraping/PY04-testCSV.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open("../carviewer.html") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
-
-print(soup.tr)
