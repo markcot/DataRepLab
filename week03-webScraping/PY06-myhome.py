d92ada6d0314c489b914dbea6c2d72e8db277774
markcot <g00376335@gmit.ie>
2020-10-16 20:34:41 +0100

    L3b step 18

--- week03-webScraping/PY06-myhome.py
+++ week03-webScraping/PY06-myhome.py
@@ -1,8 +1,12 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 page = requests.get("https://www.myhome.ie/residential/mayo/property-for-sale?page=1")
 
 soup = BeautifulSoup(page.content, 'html.parser')
+
+home_file = open('week03MyHome.csv', mode='w')
+home_writer =csv.writer(home_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
 listings = soup.findAll("div", class_="PropertyListingCard")
 
@@ -14,4 +18,5 @@ for listing in listings:
     address = listing.find(class_="PropertyListingCard__Address").text
     entry.append(address)
 
-    print(entry)
+    home_writer.writerow(entry)
+home_file.close()
