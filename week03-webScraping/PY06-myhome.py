ad384175195d374dd990fb5ce46cec6c8c0c3d73
markcot <g00376335@gmit.ie>
2020-10-16 20:21:05 +0100

    L3b step 13 & 14

--- week03-webScraping/PY06-myhome.py
+++ week03-webScraping/PY06-myhome.py
@@ -3,4 +3,7 @@ from bs4 import BeautifulSoup
 page = requests.get("https://www.myhome.ie/residential/mayo/property-for-sale?page=1")
 
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+
+listings = soup.find("div", class_="PropertyListingCard")
+
+print(listings)
