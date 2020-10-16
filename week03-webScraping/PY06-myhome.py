8818cec1744f0f6cf314ea1b0787baf52c37c81e
markcot <g00376335@gmit.ie>
2020-10-16 20:15:43 +0100

    L3b step 11 & 12

---
+++ week03-webScraping/PY06-myhome.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+page = requests.get("https://www.myhome.ie/residential/mayo/property-for-sale?page=1")
+
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())
