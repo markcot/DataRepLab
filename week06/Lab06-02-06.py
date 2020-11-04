177f39324d933b3b53bb8ad4de6f90411bfaaa98
markcot <g00376335@gmit.ie>
2020-11-04 21:16:55 +0000

    Lab06-02-06 & 07 complete. token for 6 not working.

---
+++ week06/Lab06-02-06.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+
+# remove the minus sign
+apiKey = '7aa146eafee094d3a7b1e81aa1d8fcb0eec8b91-0'
+url = 'https://api.github.com/repos/datarepresentationstudent/aPrivateOne'
+filename = "repo.json"
+
+response = requests.get(url, auth=('token',apiKey))
+
+repoJSON = response.json()
+#print(response.json())
+
+file = open(filename, 'w')
+json.dump(repoJSON, file, indent=4)
---
+++ week06/Lab06-02-07.py
@@ -0,0 +1,7 @@
+import requests
+
+url = 'https://github.com/markcot/jupyter-example/blob/master/README.md'
+
+response = requests.get(url)
+
+print(response.content)
