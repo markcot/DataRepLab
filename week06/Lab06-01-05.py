f0065d3545255ae43d187c7fc2b521444013cd54
markcot <g00376335@gmit.ie>
2020-11-04 09:24:48 +0000

    Lab06-01-05c complete. Only 4 datafiels/columns extracted to excel show that it works

--- week06/Lab06-01-05.py
+++ week06/Lab06-01-05.py
@@ -1,4 +1,5 @@
 import requests, json
+from xlwt import *
 
 #url = "https://api.github.com/users?since=100"
 url = "https://api.github.com/users/andrewbeattycourseware/followers"
@@ -11,3 +12,21 @@ data = response.json()
 filename = 'githubusers.json'
 with open(filename, 'w') as f:
    json.dump(data, f, indent=4)
+
+
+#write to excel file
+w = Workbook()
+ws = w.add_sheet('githubusers')
+row = 0
+ws.write(row, 0, "login")
+ws.write(row, 1, "id")
+ws.write(row, 2, "url")
+ws.write(row, 3, "site_admin")
+row += 1
+for user in data:
+   ws.write(row, 0, user["login"])
+   ws.write(row, 1, user["id"])
+   ws.write(row, 2, user["url"])
+   ws.write(row, 3, user["site_admin"])
+   row += 1
+w.save('githubusers.xls')
