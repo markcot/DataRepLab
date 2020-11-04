49098cf3986a4eb1bfdaea50814bea17c8530f37
markcot <g00376335@gmit.ie>
2020-11-04 09:09:40 +0000

    Lab06-01-04a Andrew follow list complete

--- week06/Lab06-01-05.py
+++ week06/Lab06-01-05.py
@@ -1,6 +1,7 @@
 import requests, json
 
-url = "https://api.github.com/users?since=100"
+#url = "https://api.github.com/users?since=100"
+url = "https://api.github.com/users/andrewbeattycourseware/followers"
 
 response = requests.get(url)
 data = response.json()
