be51a5f289ca5bdee2c614e5ec75319eca94a872
markcot <g00376335@gmit.ie>
2020-11-04 09:14:51 +0000

    Lab06-01-04b complete

--- week06/Lab06-01-05.py
+++ week06/Lab06-01-05.py
@@ -5,4 +5,9 @@ url = "https://api.github.com/users/andrewbeattycourseware/followers"
 
 response = requests.get(url)
 data = response.json()
-print(data)
+#print(data)
+
+# Get the file name for the new file to write
+filename = 'githubusers.json'
+with open(filename, 'w') as f:
+   json.dump(data, f, indent=4)
