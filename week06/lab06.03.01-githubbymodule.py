b5bfd999cef484f466dad5658550c1a24b0884d4
markcot <g00376335@gmit.ie>
2020-11-07 19:29:28 +0000

    L06-3 steps 5 to 6 complete

--- week06/lab06.03.01-githubbymodule.py
+++ week06/lab06.03.01-githubbymodule.py
@@ -8,4 +8,8 @@ g = Github("71b89bdc0378f2330931041ef2f2fc5730433365")
 #    print(repo.name)
 
 repo = g.get_repo("datarepresentationstudent/aPrivateOne")
-print(repo.clone_url)
+# print(repo.clone_url)
+
+fileInfo = repo.get_contents("test.txt")
+urlOfFile = fileInfo.download_url
+# print(urlOfFile)
