4bea7dbe87b07ca2b661dd100f1b986d879fc44b
markcot <g00376335@gmit.ie>
2020-11-07 19:38:23 +0000

    L06-3 steps 7 to 9 complete

--- week06/lab06.03.01-githubbymodule.py
+++ week06/lab06.03.01-githubbymodule.py
@@ -1,8 +1,9 @@
 from github import Github
+import requests
 
 # remove the minus sign from the key
 #g = Github("7aa146eafee094d3a7b1e81aa1d8fcb0eec8b91-0")
-g = Github("71b89bdc0378f2330931041ef2f2fc5730433365")
+g = Github("71b89bdc0378f2330931041ef2f2fc573043336-5")
 
 # for repo in g.get_user().get_repos():
 #    print(repo.name)
@@ -13,3 +14,13 @@ repo = g.get_repo("datarepresentationstudent/aPrivateOne")
 fileInfo = repo.get_contents("test.txt")
 urlOfFile = fileInfo.download_url
 # print(urlOfFile)
+
+response = requests.get(urlOfFile)
+contentOfFile = response.text
+# print(contentOfFile)
+
+newContents = contentOfFile + " more stuff Mark \n"
+# print(newContents)
+
+gitHubResponse = repo.update_file(fileInfo.path,"updated by Mark",newContents,fileInfo.sha)
+print(gitHubResponse)
