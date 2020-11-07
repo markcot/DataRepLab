70d407586500700be797c904620dc54cbb659a8b
markcot <g00376335@gmit.ie>
2020-11-07 19:26:19 +0000

    L06-3 steps 1 to 4 complete

---
+++ week06/lab06.03.01-githubbymodule.py
@@ -0,0 +1,11 @@
+from github import Github
+
+# remove the minus sign from the key
+#g = Github("7aa146eafee094d3a7b1e81aa1d8fcb0eec8b91-0")
+g = Github("71b89bdc0378f2330931041ef2f2fc5730433365")
+
+# for repo in g.get_user().get_repos():
+#    print(repo.name)
+
+repo = g.get_repo("datarepresentationstudent/aPrivateOne")
+print(repo.clone_url)
