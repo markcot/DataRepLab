d5affd14fd841e94e9ef9ad56ee7b9e530e13a95
markcot <g00376335@gmit.ie>
2020-11-25 21:15:23 +0000

    Lab09 Steps 1 to 4 complete

---
+++ week09-mysql/lab09.02.02.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+   host="localhost",
+   user="root",
+   password="Root147!",
+   database="datarepresentation"
+)
+
+mycursor = mydb.cursor()
+sql="CREATE TABLE student (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT)"
+
+mycursor.execute(sql)
